81e290523b0ed4724d5c509fd65b053af23af08a
Vasilis Tsilidis <btsilidis@hotmail.com>
2024-05-16 19:26:18 +0300

    delete 1 file and update 1 file

--- src/MyPack.jl
+++ src/MyPack.jl
@@ -1,5 +1,20 @@
 module MyPack
 
-# Write your package code here.
+export func, my_print
+
+"""
+    func(x)
+
+Return double the number `x` plus `1`.
+"""
+func(x) = 2x + 1
+
+
+"""
+    my_print()
+
+Print "Hello world!".
+"""
+my_print() = println("Hello world!")
 
 end
